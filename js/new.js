589b8ad76086a8d79ab9f02c086faf7dca231ba0
Teddy Austin Tobenna <136184722+Tobennaaustin@users.noreply.github.com>
2024-05-04 15:04:10 +0100

    Update new.js
    I made some changes

--- js/new.js
+++ js/new.js
@@ -31,7 +31,7 @@ form.addEventListener('submit', e => {
 
     setTimeout(() => {
         modal.style.display = 'none';
-        modal.classList.remove('close-modal'); // Remove the fadeout class for subsequent modals
+        modal.classList.add('close-modal'); // Remove the fadeout class for subsequent modals
     }, 1000);
 });
 
